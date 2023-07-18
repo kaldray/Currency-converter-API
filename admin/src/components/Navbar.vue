b0aa080cdc46a264cc99ae1d12b08ba60d19ed6d
kaldray <jordray@protonmail.com>
2023-07-18 18:42:15 +0200

    feat: setup vue-router and vuetify

---
+++ admin/src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { computed, unref } from "vue";
+import { useDisplay } from "vuetify";
+
+const display = useDisplay();
+const isSmartphone = computed(() => {
+  return unref(display.xs);
+});
+
+const routes = [{ to: "/", text: "Accueil" }];
+</script>
+
+<template>
+  <v-app-bar
+    color="blue"
+    scroll-behavior="elevate"
+  >
+    <v-app-bar-title>Currency</v-app-bar-title>
+    <!-- Menu Mobile -->
+    <v-menu v-if="isSmartphone">
+      <template v-slot:activator="{ props }">
+        <v-app-bar-nav-icon v-bind="props" />
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="{ to, text } in routes"
+          :to="to"
+          :key="to"
+        >
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <!-- Menu Desktop -->
+    <v-toolbar-items v-else>
+      <template
+        v-for="{ to, text } in routes"
+        :key="to"
+      >
+        <v-btn
+          :to="to"
+          variant="text"
+          >{{ text }}</v-btn
+        >
+        <v-divider vertical></v-divider>
+      </template>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "@/router";
 
-createApp(App).mount('#app')
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const app = createApp(App);
+
+app.use(
+  createVuetify({
+    components,
+    directives,
+  }),
+);
+
+app.use(router);
+
+app.mount("#app");
