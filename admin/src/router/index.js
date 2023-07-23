77cfb0e70c6e50c54f78111f9bf261f68db047b3
kaldray <jordray@protonmail.com>
2023-07-23 14:52:07 +0200

    refactoring: change the before each to before enter for each route for more granularity

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -3,44 +3,89 @@ import HomeView from "@/views/HomeView.vue";
 import AdminView from "@/views/AdminView.vue";
 import UpdateView from "@/views/UpdateView.vue";
 import CreateView from "@/views/CreateView.vue";
+import DocsView from "@/views/DocsView.vue";
 import { useAuthStore } from "@/store";
 
+/**
+ * @typedef {import('vue-router').RouteRecordNormalized} RouteRecordNormalized
+ * @typedef {import('vue-router').RouteRecordName} RouteRecordName
+ * @typedef {import('vue-router')._RouteRecordBase} RouteRecordBase
+ */
+
+/**
+ *
+ * @param {RouteRecordNormalized} from
+ * @returns {{name:RouteRecordName}}
+ */
+const isAuthenticated = (from) => {
+  const authStore = useAuthStore();
+  if (authStore.user === null) {
+    return { name: from.name };
+  }
+};
+
+/**
+ *
+ * @type {RouteRecordBase[]}
+ */
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "Login",
     component: HomeView,
   },
   {
     path: "/list",
     name: "Admin",
     component: AdminView,
+    /**
+     *
+     * @param {RouteRecordNormalized} to
+     * @param {RouteRecordNormalized} from
+     * @returns {{name:RouteRecordName}}
+     */
+    beforeEnter: (to, from) => {
+      return isAuthenticated(from);
+    },
   },
   {
     path: "/update",
     name: "AdminUpdate",
     component: UpdateView,
+    /**
+     *
+     * @param {RouteRecordNormalized} to
+     * @param {RouteRecordNormalized} from
+     * @returns {{name:RouteRecordName}}
+     */
+    beforeEnter: (to, from) => {
+      return isAuthenticated(from);
+    },
   },
   {
     path: "/create",
     name: "AdminCreate",
     component: CreateView,
+    /**
+     *
+     * @param {RouteRecordNormalized} to
+     * @param {RouteRecordNormalized} from
+     * @returns {{name:RouteRecordName}}
+     */
+    beforeEnter: (to, from) => {
+      return isAuthenticated(from);
+    },
+  },
+  {
+    path: "/docs",
+    name: "Docs",
+    component: DocsView,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
-
-router.beforeEach(async (to, from) => {
-  const authStore = useAuthStore();
-  if (authStore.user === null && to.name !== "Home") {
-    return { name: "Home" };
-  }
-  if (authStore.user !== null && from.name !== "Home" && to.name === "Home") {
-    return { name: "Admin" };
-  }
 });
 
 export { router };
