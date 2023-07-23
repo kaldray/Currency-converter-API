adae528353e84be1934194792203ec8e6d81362d
kaldray <jordray@protonmail.com>
2023-07-23 16:58:15 +0200

    feat: return JSON for not found route

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -3,28 +3,33 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * The list of the inputs that are never flashed to the session on validation exceptions.
+   *
+   * @var array<int, string>
+   */
+  protected $dontFlash = ["current_password", "password", "password_confirmation"];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+  /**
+   * Register the exception handling callbacks for the application.
+   */
+  public function register(): void
+  {
+    $this->renderable(function (NotFoundHttpException $e, Request $request) {
+      if ($request->is("api/*")) {
+        return response()->json(
+          [
+            "message" => "La ressource demandée n'existe pas.",
+          ],
+          404
+        );
+      }
+    });
+  }
 }
